5404335e90cb8ec40a845c99cca00c0f33682316
abutler911 <abutler911@gmail.com>
2023-03-25 11:42:36 -0600

    added investment charts

--- index.js
+++ index.js
@@ -93,6 +93,19 @@ const investments = [
   { name: "Robinhood", amount: 1000, image: "images/robinhood.png" },
 ];
 
+const initialInvestmentData = [
+  { month: "March 2023", amount: 1000 },
+  { month: "April 2023", amount: 5000 },
+  { month: "May 2023", amount: 7000 },
+  { month: "June 2023", amount: 9000 },
+  { month: "July 2023", amount: 11000 },
+  { month: "August 2023", amount: 13000 },
+  { month: "September 2023", amount: 15000 },
+  { month: "October 2023", amount: 17000 },
+  { month: "November 2023", amount: 19000 },
+  { month: "December 2023", amount: 21000 },
+];
+
 Chart.register(ChartDataLabels);
 
 // Add cards to the container
@@ -257,6 +270,119 @@ const debtByMonthChart = new Chart(ctxByMonth, {
   },
 });
 
+const investmentChartData = {
+  labels: investments.map((investment) => investment.name),
+  datasets: [
+    {
+      label: "Investment Amount",
+      data: investments.map((investment) => investment.amount),
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(255, 206, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(153, 102, 255, 0.2)",
+      ],
+      borderColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(153, 102, 255, 1)",
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+const investmentCtx = document
+  .getElementById("investmentGraph")
+  .getContext("2d");
+new Chart(investmentCtx, {
+  type: "bar",
+  data: investmentChartData,
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+    plugins: {
+      datalabels: {
+        color: "#000",
+        anchor: "end",
+        align: "top",
+        formatter: function (value) {
+          return "$" + value.toLocaleString();
+        },
+      },
+      legend: {
+        display: false,
+      },
+    },
+  },
+});
+
+const chartDataByInvestmentMonth = {
+  labels: initialInvestmentData.map((investment) => investment.month),
+  datasets: [
+    {
+      label: "Investment Amount by Month",
+      data: initialInvestmentData.map((investment) => investment.amount),
+      backgroundColor: "rgba(75, 192, 192, 0.2)",
+      borderColor: "rgba(75, 192, 192, 1)",
+      borderWidth: 1,
+    },
+  ],
+};
+
+const investmentCtxByMonth = document
+  .getElementById("investmentGraphByMonth")
+  .getContext("2d");
+const investmentByMonthChart = new Chart(investmentCtxByMonth, {
+  type: "line",
+  data: chartDataByInvestmentMonth,
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+    plugins: {
+      datalabels: {
+        color: "#000",
+        anchor: "end",
+        align: "top",
+        formatter: function (value) {
+          return "$" + value.toLocaleString();
+        },
+      },
+      legend: {
+        display: false,
+      },
+    },
+  },
+});
+
+// Calculate total investments
+const totalInvestments = investments.reduce(
+  (acc, investment) => acc + investment.amount,
+  0
+);
+
+// Update the h2 element with the calculated value
+document.getElementById(
+  "total-investments"
+).innerText = `Total Investments: $${totalInvestments.toLocaleString()}`;
+
+function updateInvestmentAmounts(newInvestmentAmounts) {
+  investmentByMonthChart.data.datasets[0].data = newInvestmentAmounts;
+  investmentByMonthChart.update();
+}
+
+updateInvestmentAmounts([1000]); // Replace these values with the updated investment amounts, first amount is for March 2023
 function updateDebtAmounts(newDebtAmounts) {
   debtByMonthChart.data.datasets[0].data = newDebtAmounts;
   debtByMonthChart.update();
